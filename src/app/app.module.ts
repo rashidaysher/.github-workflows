228f6bd2ee9c48e9c9310e49a5f86dce5f2c489b
rashidaysher <aisha.ahmed@student.moringaschool.com>
2021-10-21 11:37:53 +0300

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { GitComponent } from './git/git.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    GitComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
